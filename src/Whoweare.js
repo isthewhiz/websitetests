a0f7378f5ee7086510f479dc857bea1c02699af7
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-08-13 16:45:02 -0700

    fix styles and component

--- src/Whoweare.js
+++ src/Whoweare.js
@@ -6,6 +6,7 @@ import "./Whoweare.scss";
 // this can be in its own fiile
 const collectiveConfig = [
   {
+    // image with a target url
     photo:
       "https://mappingaction.files.wordpress.com/2019/12/sachi_headshot-e1575161201903.jpg?w=300&h=278",
     bioName: "Sachi Arakawa",
@@ -14,7 +15,7 @@ const collectiveConfig = [
   },
 
   {
-    photo: pandaImage,
+    photo: pandaImage, // local image
     bioName: "Tim Hitchins",
     text:
       " is a GIS developer with an obsession for interactive mapping, data analytics and everything in between. Since his early 20s, Tim has participated in various grassroots social justice, environmental and animal liberation causes. These days Tim spends his time working with community partners on projects that bridge the gap between analytics and activism. Tim graduated with an MS in Geography from Portland State University and is currently a GIS instructor at Portland Community College.",
@@ -26,27 +27,25 @@ class Whoweare extends Component {
     return (
       <nav className="nav-outer-container">
         <div>
-          <ul id="collective">
-            {collectiveConfig.map((item) => (
-              <div>
-                <img
-                  src={item.photo}
-                  alt="An image of a collective member."
-                ></img>
-                {/* To make a list do this below*/}
-                {/*<ul>
+          {collectiveConfig.map((item) => (
+            <div>
+              <img
+                src={item.photo}
+                alt="An image of a collective member."
+              ></img>
+              {/* To make a list do this below*/}
+              {/*<ul>
                   <li>{item.bioName}</li>
                   <li>{item.text}</li>
                 </ul> 
                 */}
-                {/* To make it seemless do this below*/}
-                <div>
-                  {item.bioName}
-                  {item.text}
-                </div>
+              {/* To make it seemless do this below*/}
+              <div>
+                {item.bioName}
+                {item.text}
               </div>
-            ))}
-          </ul>
+            </div>
+          ))}
         </div>
       </nav>
     );
